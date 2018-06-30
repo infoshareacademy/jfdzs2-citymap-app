b2e240c6b05b94bedab553c8e46d79e540b68314
katarzyna-stepczynska <stepczynska.katarzyna@gmail.com>
2018-06-30 16:30:57 +0200

    Added some category statics button and login button

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import {GoogleApiWrapper} from 'google-maps-react'
 import MapContainer from './components/MapContainer'
 import Header from "./components/header/header"
+import { Link } from 'react-router-dom'
 // import Section from "./components/section/section"
-import Footer from "./components/footer/footer"
-import Menu from "./components/menu/menu"
+// import Footer from "./components/footer/footer"
+// import Menu from "./components/menu/menu"
 import {
     Collapse,
     Navbar,
@@ -28,16 +29,20 @@ class App extends Component {
                 <MapContainer google={this.props.google}/>
                 <Header/>
                 <div className="search-wrapper">
-                <input type="text" className="form-control" placeholder="Wpisz szukane miejsce" />
-                    lub wybierz kategorię:
+                    <label>Znajdź:</label>
+                <input type="text" className="form-control" placeholder="Wpisz szukane miejsce..." />
+                    <h4>lub wybierz kategorię: </h4>
                     <ul>
                         <li>Parki</li>
-                        <li>
-                            Restauracje
-                        </li>
+                        <li>Restauracje</li>
+                        <li>Kawiarnie</li>
                         <li>Kina</li>
                         <li>Wystawy</li>
+                        <li>Obiekty sportowe</li>
+                        <li>Imprezy</li>
                     </ul>
+                    <Link to="./"><button type="button" className="btn btn-default btn-log">Statystyki</button></Link>
+                    <Link to="./"><button type="button" className="btn btn-default btn-log">Zaloguj</button></Link>
                 </div>
                 {/*<Menu />*/}
             </div>
