fa66597c0f8135b4c248a4fcdafb3fb8e34ceee8
katarzyna-stepczynska <stepczynska.katarzyna@gmail.com>
2018-07-10 20:26:37 +0200

    Delete Route in index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import CurrentPosition from "./components/section/CurrentPosition";
 import Results from "./routes/results/results";
 import User from "./routes/user/user";
 import Favourite from "./routes/fav/fav";
@@ -12,7 +11,6 @@ ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={App} />
-      <Route exact path="/CurrentPosition" component={CurrentPosition} />
       <Route path="/fav" component={Favourite} />
       <Route path="/results" component={Results} />
       <Route path="/user" component={User} />
