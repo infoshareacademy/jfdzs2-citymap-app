3304394c04187a3429e7639bd7b493645ae22ba5
JoannaDS <j.dabrowska.slaby@gmail.com>
2018-07-11 13:48:43 +0200

    Changes after merging

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 import Results from "./routes/results/results";
 import User from "./routes/user/user";
 import Favourite from "./routes/fav/fav";
-<<<<<<< HEAD
 import Login from "./Login/Login";
-=======
-import registerServiceWorker from './registerServiceWorker';
->>>>>>> dd31f04813e85f4bdab8668aa5c4c9d2563c3d39
 
 ReactDOM.render(
   <BrowserRouter>
@@ -22,5 +19,6 @@ ReactDOM.render(
       <Route path="/login" exact component={Login} />
     </Switch>
   </BrowserRouter>,
-  document.getElementById("root"));
+  document.getElementById("root")
+);
 registerServiceWorker();
